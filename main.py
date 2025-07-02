08d8789cf1c6519cc22747a10445dd701dcc5e40
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-07-02 14:05:22 +0500

    refactor: tabs added

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 import streamlit as st
 
-st.title("My Business Info")
+st.set_page_config(page_title="Business Info", page_icon="💼", layout="wide")
 
-st.header("Working Hours")
-st.write("Monday to Friday: 9 AM – 6 PM")
-
-st.header("Location")
-st.write("Uzbekistan, Tashkent")
-
-st.header("Phone Number")
-st.write("+998 91 339 44 54")
-
-st.header("Email Address")
-st.write("shokhrukhmirzo_usmonov@student.itpu.uz")
+overview_tab, working_hours_tab, location_tab, contact_tab, ask_us_tab = st.tabs(["🏢 Overview", "⏰ Working Hours", "📍 Location", "📞 Contact", "💬 Ask Us"])
