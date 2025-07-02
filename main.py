31de75f3407b2502f726ce1350f3e8e03f13529f
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-07-02 10:56:39 +0500

    feat(main): Business Info implemented

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("My Business Info")
+
+st.header("Working Hours")
+st.write("Monday to Friday: 9 AM – 6 PM")
+
+st.header("Location")
+st.write("Uzbekistan, Navoiy")
+
+st.header("Phone Number")
+st.write("+998 91 339 44 54")
+
+st.header("Email Address")
+st.write("shokhrukhmirzo_usmonov@student.itpu.uz")
